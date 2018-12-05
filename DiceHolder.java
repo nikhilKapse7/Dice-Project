27b303ea901ea32492593e472f75cd2215e2a738
nikhilKapse7 <45395116+nikhilKapse7@users.noreply.github.com>
2018-12-05 09:32:39 -0500

    Create DiceHolder.java
    Created file with methods that add die, shake dice, and print out the dice in the container.

---
+++ DiceHolder.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+public class DiceHolder {
+	private ArrayList<Die> container;
+
+	public DiceHolder(){
+		for(int i = 0; i < 6; i++){
+			container.add(new Die());
+		}
+	}
+
+
+	public int addDie(Die die) {
+		//adds die
+		if(container.size() >= 6) {
+			return -1;
+		}
+		else if( container.size() < 6) {
+			container.add(die);
+			return 1;
+		}
+	}
+
+
+	public void shake() {
+		//shakes up dice in container
+		for(int i = 0; i < container.size(); i++) {
+			roll(container.get(i));
+		}
+	}
+
+
+	public String toString() {
+		//prints out all dice
+		String allDice;
+		for(int i = 0; i < container.size(); i++) {
+				allDice += container.get(i) + "\n";
+		}
+		return allDice;
+	}
+}
