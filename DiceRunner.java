2650b515d388ca97b216c27ded58d140a21a5734
pvirtudazo <pvirtudazo7@gmail.com>
2018-12-05 09:23:38 -0500

    DiceRunner update
    fixed DiceRunner class to output correctly

--- DiceRunner.java
+++ DiceRunner.java
@@ -6,17 +6,23 @@ public class DiceRunner
 		Die die1=new Die();
 		Die die2=new Die();
 		int count=0;
+		int count2=1;
 		while (count>-1)
 		{
 			die1.roll();
 			die2.roll();
-			die1.toString();
-			die2.toString();
+			System.out.println("roll "+count2);
+			System.out.println(die1.toString());
+			System.out.println(die2.toString());
 			if (die1.getValue()==die2.getValue())
 			{
+				System.out.println();
 				System.out.println("Snake eyes!");
+				System.out.println("die 1 has "+die1.getSides()+" sides and a value of "+die1.getValue());
+				System.out.println("die 2 has "+die2.getSides()+" sides and a value of "+die2.getValue());
 				count--;
 			}
+			count2++;
 		}
 	}
 }
